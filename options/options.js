cfec0027c206784a2ae855533e61be2bccc58c94
Crowdbotics <team@crowdbotics.com>
2021-09-13 03:49:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dream_again_financi_30493Identifier",
+  url: "https://dream_again_financi_30493Identifier.botics.co",
+  api: "https://dream_again_financi_30493Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
